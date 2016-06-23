6209230aca757cf726fc31faafc8211f180136e0
Joesty <aktan1812@gmail.com>
2016-06-23 22:55:49 +0300

    route windows

--- app/components/attachContainer/attachContainer.controller.js
+++ app/components/attachContainer/attachContainer.controller.js
@@ -1,6 +1,47 @@
 export default class AttachContainerController {
-  constructor() {
+  constructor($scope, $location) {
     this.name = 'attachContainer';
+    $scope.WebSocketTest = function(id)
+     {
+        if ("WebSocket" in window)
+        {
+           alert("WebSocket is supported by your Browser!");
+
+           // Let us open a web socket
+           $scope.ws = new WebSocket('ws://119.vlabs.knu.ua:4243/containers/'+id+'/attach/ws?stream=1');
+
+           $scope.ws.onopen = function()
+           {
+           };
+
+           $scope.ws.onmessage = function (evt)
+           {
+              var received_msg = evt.data;
+            document.getElementById('123').innerHTML += evt.data + '<br/>';
+           };
+
+           $scope.ws.onclose = function()
+           {
+              // websocket is closed.
+              alert("Connection is closed...");
+           };
+        }
+
+        else
+        {
+           // The browser doesn't support WebSocket
+           alert("WebSocket NOT supported by your Browser!");
+        }
+     }
+    $scope.senddata = function()
+     {
+            var sendData = document.getElementById('123input').value;
+              $scope.ws.send(sendData + "\n");
+    }
     
+    $scope.id = $location.search().id.split("'")[0];
+      
+      console.log($scope.id.split("'"));
   }
 }
+AttachContainerController.$inject = ['$scope','$location'];
